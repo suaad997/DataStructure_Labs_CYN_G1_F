bd3fce1fe7ef1333e15f56349a8b8fb092a4f699
suada <suadaljuaid1@gmail.com>
2025-12-09 21:46:29 +0300

    lab6-Singlylinkedlist

--- src/SinglyLinkedList/Main.java
+++ src/SinglyLinkedList/Main.java
@@ -1,4 +1,14 @@
 package SinglyLinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList<Integer> list=new SinglyLinkedList<Integer>();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.removeLast();
+        list.display();
+
+
+    }
 }
--- src/SinglyLinkedList/SinglyLinkedList.java
+++ src/SinglyLinkedList/SinglyLinkedList.java
@@ -14,6 +14,74 @@ public boolean isEmpty(){
 
 }
 
+public void addFirst(E data){
+    Node<E> newNode=new Node(data,head);
+
+    head=newNode;
+    if (isEmpty())
+        tail=head;
+    size++;
+}
+public E getFirst(){
+    if(isEmpty()) return null;
+    return head.getData();
+}
+public E removeFirst(){
+    E delete = head.getData();
+    head=head.getNext();
+    size--;
+    if (isEmpty())
+        tail=head;
+    return delete;
+}
+public void addLast(E data){
+    Node<E> newNode=new Node(data,null);
+    if (isEmpty())
+        head=newNode;
+    else
+        tail.setNext(newNode);
+
+    tail=newNode;
+    size++;
+}
+public E getLast(){
+        if(isEmpty()) return null;
+        return tail.getData();
+    }
+public E removeLast(){
+    if(isEmpty())return null;
+    E delete= tail.getData();
+    if (head==tail){
+        head=null;
+        tail=null;
+    }
+    else {
+        Node<E> temp=head;
+        while (temp.getNext()!=tail){
+            temp=temp.getNext();
+        }
+        temp.setNext(null);
+        tail=temp;
+    }
+    size--;
+    return delete;
+}
+public void display(){
+    Node<E> temp=head;
+
+    while (temp!=null){
+        System.out.print(temp.getData()+"---->");
+        temp=temp.getNext();
+    }
+    System.out.println(" null\n");
+}
+
+
+
+
+
+
+
 
 class Node<E>{
     private E data;
